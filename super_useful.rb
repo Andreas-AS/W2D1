df09c40b00cc6e53f4d639e3a7c7b3c96f7f0235
Andreas <andreas.argyris@gmail.com>
2019-02-12 08:58:48 -0500

    Play with classes and OOP

---
+++ super_useful.rb
@@ -0,0 +1,96 @@
+# PHASE 2
+
+
+def convert_to_int(str)
+  begin
+    Integer(str)
+  rescue ArgumentError
+    puts "Use an integer"
+    retry
+  end
+end
+
+class CoffeeError < StandardError ; end
+# PHASE 3
+FRUITS = ["apple", "banana", "orange"]
+
+def reaction(maybe_fruit)
+  begin
+    if FRUITS.include? maybe_fruit
+      puts "OMG, thanks so much for the #{maybe_fruit}!"
+    elsif maybe_fruit == "coffee"
+      raise CoffeeError
+    else 
+      raise StandardError
+    end
+  rescue CoffeeError
+      FRUITS << "coffee"
+      puts "do you really want coffee?"
+      feed_me_a_fruit
+  end
+end
+
+def feed_me_a_fruit
+  puts "Hello, I am a friendly monster. :)"
+
+  puts "Feed me a fruit! (Enter the name of a fruit:)"
+  maybe_fruit = gets.chomp
+  reaction(maybe_fruit) 
+end  
+
+# PHASE 4
+class YearsError < StandardError ; end
+class ActivityError < StandardError ; end
+class NameError < StandardError ; end
+
+class BestFriend
+
+  
+  def initialize(name, yrs_known, fav_pastime)
+    
+    @name = name
+    
+    begin
+      raise NameError if @name.length < 1
+    rescue NameError
+      puts "Your friend needs a name"
+      @name = gets.chomp
+      retry
+    end
+
+    @yrs_known = yrs_known
+
+    begin
+    raise YearsError if @yrs_known < 5
+    rescue YearsError
+      @yrs_known = 5
+      puts "A real friendship is minimum 5 years long"
+    retry
+    end
+
+    @fav_pastime = fav_pastime
+    begin
+      raise ActivityError if @fav_pastime.length < 1
+    rescue ActivityError
+      puts "What do you like to do together?"
+      @fav_pastime = gets.chomp
+      retry
+    end
+  
+  end
+
+  def talk_about_friendship
+
+    puts "Wowza, we've been friends for #{@yrs_known}. Let's be friends for another #{1000 * @yrs_known}."
+  end
+
+  def do_friendstuff
+    puts "Hey bestie, let's go #{@fav_pastime}. Wait, why don't you choose. 😄"
+  end
+
+  def give_friendship_bracelet
+    puts "Hey bestie, I made you a friendship bracelet. It says my name, #{@name}, so you never forget me." 
+  end
+end
+
+
